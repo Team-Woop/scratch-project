4951f1bf6e1af2f1fa9e4aa75de0477f2c1357f2
Garrett Layden <garlayden@gmail.com>
2022-05-27 19:07:53 -0400

    test

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
 const help = 'help';
+
+/*
+
+git:([name]) git add . 
+git:([name]) git commit -m "[message here]"
+git:([name]) git push origin [name]
+git:([name]) git checkout Dev
+git:(Dev) git pull origin [name]
+*/
