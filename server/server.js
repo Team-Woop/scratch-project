99deb92380ff73eec4a2ec6fcd21e82e14f6cbc0
Michael Villamor <77362610+mike-villamor-git@users.noreply.github.com>
2022-05-28 16:31:59 -0400

    Revert "J hauanio"

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ const mongoURI = 'mongodb://localhost:27017'
 
 mongoose.connect(mongoURI);
 
-const controller = require('./controllers/controller.js');
 const { fileURLToPath } = require('url');
 
 app.use(express.json());
@@ -15,26 +14,10 @@ app.use(express.json());
 
 app.use('/', express.static(path.resolve(__dirname, '../build')));
 
-app.get('/', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
 // join styles.css page
 app.get('/client/styles.css', (req, res) => {
   res.status(200).setHeader('content-type', 'text/css').sendFile(path.join(__dirname, '../client/styles.css'));
 });
-
-// receive input from client in format: 
-// {startLoc : {city:city, state:state}, destLoc : {city:city, state: state}, totalFuelCap: fuelCap}
-// returns in format: {Distance : Distance, Cost : Cost}
-
-//  https://maps.googleapis.com/maps/api/geocode/
-//  json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyArkv_B14HtFM54IbcygLMLwVY3PGQYjRI
-app.get('/submit', controller.getSteps, controller.getPrice, (req, res) => {
-  res.send(res.locals.gas);
-})
-
-// request to google directions api for legs
-app.get('')
-
 
 // global error handler - only invoked when next passes in an arg
 app.use((err, req, res, next) => {
