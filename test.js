787f6102c1cd01ae04af9e013de1a6b3619fb60b
Garrett Layden <garlayden@gmail.com>
2022-05-27 20:03:18 -0400

    update

--- test.js
+++ test.js
@@ -1,8 +1,13 @@
 const help = 'help';
 const help2 = 'help2'
 
+// git checkout dev
+// git pull origin dev
+// git checkout [your branch]
+// git merge dev
 /*
 
+<<<<<<< HEAD
 git:([name]) git add . 
 git:([name]) git commit -m "[message here]"
 git:([name]) git push origin [name]
@@ -10,3 +15,14 @@ git:([name]) git checkout Dev
 git:(Dev) git pull origin [name]
 */
 // hey
+=======
+git:([your branch]) git add . 
+git:([your branch]) git commit -m "[message here]"
+git:([your branch]) git push origin [your branch]
+git:([your branch]) git checkout Dev
+git:(Dev) git pull origin [your branch]
+*/
+
+// cmd + shift + L => highlights/allows changes to 
+// everything with the same name
+>>>>>>> 3f9d56a (help #2)
