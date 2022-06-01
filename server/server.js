616eb39f1d735b7e73ca8f301a10a9f886b2b231
Joseph Borrow <joe@Josephs-MacBook-Air.local>
2022-06-01 06:37:45 -0400

    removed multiple css references and setup in index.js to remove error msg, consolidated backend setup to webpack

--- server/server.js
+++ server/server.js
@@ -1,27 +1,41 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const mongoose = require('mongoose')
-const mongoURI = 'mongodb://localhost:27017'
-// mongodb+srv://JHauanio:<password>@cluster0.sqhld.mongodb.net/?retryWrites=true&w=majority
-
-mongoose.connect(mongoURI);
-
+const port = 3000;
 const controller = require('./controllers/controller.js');
 const { fileURLToPath } = require('url');
 
-app.use(express.json());
-// app.use(express.urlencoded());
+// This MongooseDB boilerplate was causing an error when spinning up back-end
+// Commented out for now, but can re-incorporate later after we setup our own DB
+// **************** MongoDb Setup Boilerplate ********************
+// const mongoose = require('mongoose')
+// const mongoURI = 'mongodb://localhost:27017'
+// mongodb+srv://JHauanio:<password>@cluster0.sqhld.mongodb.net/?retryWrites=true&w=majority
+/// test
+// mongoose.connect(mongoURI);
+// **************** MongoDb Setup Boilerplate ********************
 
-app.use('/', express.static(path.resolve(__dirname, '../build')));
+app.use(express.json());
+
+// This can be potentially be used in production when webpack-dev-server not in use
+// TODO: wrap this section in if-else statement, using NODE_ENV to determine if code running 
+// in production or development environment
+// app.use(express.urlencoded());
+// app.use('/', express.static(path.resolve(__dirname, '../build')));
 
 app.get('/', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
 });
+
+// This can be potentially be used in production when webpack-dev-server not in use
+// TODO: wrap this section in if-else statement, using NODE_ENV to determine if code running 
+// in production or development environment
+// *************** Code for Serving CSS From Espress Directly Outside of Webpack *****************
 // join styles.css page
-app.get('/client/styles.css', (req, res) => {
-  res.status(200).setHeader('content-type', 'text/css').sendFile(path.join(__dirname, '../client/styles.css'));
-});
+// app.get('/client/styles.css', (req, res) => {
+//   res.status(200).setHeader('content-type', 'text/css').sendFile(path.join(__dirname, '../client/styles.css'));
+// });
+// *************** Code for Serving CSS From Express Directly Outside of Webpack *****************
 
 // receive input from client in format: 
 // {startLoc : {city:city, state:state}, destLoc : {city:city, state: state}, totalFuelCap: fuelCap}
@@ -34,7 +48,7 @@ app.get('/submit', controller.getSteps, controller.getPrice, (req, res) => {
 })
 
 // request to google directions api for legs
-app.get('')
+// app.get('')
 
 
 // global error handler - only invoked when next passes in an arg
@@ -50,4 +64,5 @@ app.use((err, req, res, next) => {
   return res.status(errorObj.status).send(JSON.stringify(errorObj.message));
 });
 
-app.listen(3000);
+// Setup Express to listen on port assigned to port variable above and console.log port number
+app.listen(port, () => console.log(`Listening on port ${port}`));
